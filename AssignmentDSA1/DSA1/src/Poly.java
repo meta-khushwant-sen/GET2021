90b795eb3338962323c3fb45ceda95ebb6a4cc9b
Khushwant Sen <khushwant.sen@metacube.com>
2021-03-04 22:34:03 +0530

    Assignment Submission

--- AssignmentDSA1/DSA1/src/Poly.java
+++ AssignmentDSA1/DSA1/src/Poly.java
@@ -1,18 +1,18 @@
 
-
-
 public class Poly {
 	
 	static class Node{
-		int x;
-		int y;
+		int coeff;
+		int xPower;
+		int yPower;
 		Node next;
 		
 	}
-	Node insertNode(Node head,int x,int y){
+	Node insertNode(Node head,int coeff ,int x,int y){
 		Node newNode= new Node();
-		newNode.x=x;
-		newNode.y=y;
+		newNode.coeff=coeff;
+		newNode.xPower=x;
+		newNode.yPower=y;
 		newNode.next=head;
 		head=newNode;
 		return head;
@@ -20,28 +20,33 @@ public class Poly {
 
 		
 	}
-	private  void print (Node head){
+	private  int print (Node head){
 		Node temp=head;
 		int count=0;
+		int degree;
+		int maxDegree=0;
 		while(temp!=null){
 			if(count==0){
-				System.out.print(temp.x);
+				System.out.print(temp.coeff);
 				temp=temp.next;
 				count++;
 				continue;
 			}
-			System.out.print("+"+temp.x+"*x^"+count+"*"+temp.y+"*y^"+count);
+			System.out.print("+"+temp.coeff+"x^"+temp.xPower+"*y^"+temp.yPower);
+			degree=temp.xPower+temp.yPower;
+			maxDegree=Math.max(degree,maxDegree);
 			temp=temp.next;
 			count++;
 		}
+		return maxDegree;
 	}
 	public static void main(String args[]){
 		Poly obj=new Poly();
 		Node head=null;
-		head=obj.insertNode(head,3,4);
-		head=obj.insertNode(head,1,2);
-		head=obj.insertNode(head,4,0);
-		obj.print(head);
+		head=obj.insertNode(head,3,4,2);//coefficient for degree 3
+		head=obj.insertNode(head,1,2,5);
+		head=obj.insertNode(head,4,0,0);
+		System.out.println("\nDegree of above expression: "+obj.print(head));
 		
 	}
 }
